259aeac05aad3018cd44f89a32d3d68b1dccec50
Andrzej Kornaś <enKiV24@gmail.com>
2022-10-25 05:20:33 +0200

    Connect with project 3

--- Java-learning/src/App.java
+++ Java-learning/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(Tic Tac Toe);
+        Scanner scanner = new Scanner(System.in);
+        String elo = scanner.nextLine();
+        System.out.println(elo);
+        
+        //Zmienna dla miejsc w tabeli
+        int[] place = new int[9];
+        for(int x = 0; x < 9 ; x++){
+            
+        }
+        //Druk tabeli
+        System.out.println("---+---+---");
+        System.out.print();
     }
 }

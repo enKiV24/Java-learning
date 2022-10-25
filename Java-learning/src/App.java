5c7b157b81dffbcadacf888328c6dc1c649af49e
Andrzej Kornaś <enKiV24@gmail.com>
2022-10-25 06:58:39 +0200

    Java learning 1

--- Java-learning/src/App.java
+++ Java-learning/src/App.java
@@ -3,15 +3,32 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Gra większe mniejsze");
-
-        //random
         Random random = new Random();
-        int number = random.nextInt();
-
-        //scanner
-        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(100)+1;
         
+        System.out.println("Gra Większe czy mniejsze!\nZgadnij liczbę od 1 do 100");
+        
+        Scanner scanner = new Scanner(System.in);
+        boolean correctNumber = false; 
+        
+        while(!correctNumber){
+            
+            System.out.println("Podaj liczbę");
+            
+            int guessNumber = scanner.nextInt();
+            
+            if(guessNumber < 1 || guessNumber > 100){
+                System.out.println("Podaj liczbę od 1 do 100!");
+            } else if (guessNumber < number){
+                System.out.println("Większa");
+            } else if (guessNumber > number){
+                System.out.println("Mniejsza");
+            } else if (guessNumber == number){
+                System.out.println("Brawo! Odgadłeś liczbę! Ta liczba to: " + number);
+                correctNumber = true;
+            }
+        }
+
 
     }
 }
